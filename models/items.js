a8d558a1c0be510fade73d68278a854338545ead
CharlesJonah <mutua.charles48@gmail.com>
2018-04-25 07:41:52 +0300

    adds pagination plugin to mongoose

--- models/items.js
+++ models/items.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
 
 const itemSchema = new mongoose.Schema({
     name:String,
     description:String,
     category:String
 })
-
+itemSchema.plugin(mongoosePaginate);
 const ItemModel = mongoose.model('ItemModel', itemSchema);
 
 module.exports = ItemModel;
