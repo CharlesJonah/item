be5e5d8563b0b2c4d8886dac7bcd40a4a5ab7e9d
CharlesJonah <mutua.charles48@gmail.com>
2018-04-25 07:39:29 +0300

    update mongo db connection

--- app.js
+++ app.js
@@ -16,16 +16,16 @@ if (process.env.NODE_ENV =='staging'){
 else{
   mongoose.connect(config.production.DATABASE_URL);
 }
-
 const db = mongoose.connection;
 const port = process.env.PORT || 8000;
 
-db.once('error',function(){
+db.on('error',function(){
   console.log('Failed to establish connection');
   })
 
 db.once('open',function(){
   app.listen(port);
+  console.log('Connection established');
 });
 
 app.use('/api/v1', routes);
