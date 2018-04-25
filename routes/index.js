96da65904949297d61b164dc0c9cc49adee4619f
CharlesJonah <mutua.charles48@gmail.com>
2018-04-25 07:44:39 +0300

    update resource routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
-// const items = require('./controllers/items');
+const items = require('../controllers/items');
 
-/* GET root page. */
 router.get('/', function(req, res) {
   res.status(200).send({ message: 'Welcome to Items API' });
 });
+
+router.post('/items',items.createItem);
+router.get('/items',items.getItems);
+router.get('/items/:id',items.getItem);
+router.put('/items/:id', items.updateItem);
+router.get('/categorize',  items.searchByCategory);
+router.delete('/items/:id',items.deleteItem);
+
 
 module.exports = router;
